1e260154bee3ecd97b9862a6d742bd2c91a42b89
Max <maxmillianrwoods@gmail.com>
2019-06-25 19:51:02 -0700

    challenge 2 basic requirements

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,30 @@
+var app = {
 
+  init: function () {
+    $('#app').append(`<form id='form'></form>`);
+    $('#form').append(`<input type=textarea id='text'>`);
+    $('#form').append(`<button type=submit id="submit">Submit</button>`);
+    $('#submit').on('click', app.handleSubmit);
+  },
 
+  handleSubmit: function (event) {
+    var message = {
+      text: $('#text').val()
+    };
+    $.ajax({
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        $('#text').val('');
+        app.render(data);
+      }
+    });
+    event.preventDefault();
+  },
+
+  render: function (data) {
+    $('#app').append(data);
+  }
+
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,55 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var parser = require('body-parser');
 
+var app = express();
 
+app.set('port', 3000);
+app.use(parser.json());
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.post('/', function (req, res) {
+  fs.open(path.join(__dirname + 'data.csv'), 'w', () => {
+    var body = req.body.text;
+    if (body[body.length - 1] === ';') {
+      body = body.slice(0, -1)
+    };
+    var data = JSON.parse(body);
+    var sep = ',';
+    var header = '';
+    for (key in data) {
+      if (key !== 'children') {
+        header = header + key + sep;
+      };
+    };
+    header = header.slice(0, -1) + '\n';
+    fs.appendFile('data.csv', header, () => {
+      var rows = '';
+      var recurse = function (data) {
+        if (!data.children) {
+          return;
+        }
+        var row = '';
+        for (key in data) {
+          if (key !== 'children') {
+            row = row + data[key] + sep;
+          }
+        }
+        row = row.slice(0, -1) + '\n';
+        rows = rows + row;
+        for (var i = 0; i < data.children.length; i++) {
+          recurse(data.children[i]);
+        }
+      }
+      recurse(data);
+      fs.appendFile('data.csv', rows, () => {
+        fs.readFile(path.join(__dirname + 'data.csv'), (err, data) => res.send(data));
+      });
+    });
+  });
+});
+
+app.listen(app.get('port'), () =>
+  console.log('Listening on ' + app.get('port'))
+);
